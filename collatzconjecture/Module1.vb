47c6013508f3e809f041296b3c52ad47b33ca6ba
sangmaojung <loca135728@gmail.com>
2023-09-28 08:55:43 +0800

    Example of collatz conjecture

---
+++ collatzconjecture/Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+
+Class Program
+    Public Shared Sub Main()
+        Dim i As Single = 0
+        Console.Write("Enter a positive integer: ")
+        i = Single.Parse(Console.ReadLine())
+
+        If i - CInt(i) = 0 AndAlso i > 0 Then
+            Dim n As Integer
+            n = CInt(i)
+
+            If n > 0 Then
+
+                While n <> 1
+
+                    If n Mod 2 = 0 Then
+                        n = n / 2
+                    Else
+                        n = 3 * n + 1
+                    End If
+
+                    Console.Write(" " & n)
+                End While
+
+                Console.ReadLine()
+
+            End If
+        Else
+            Console.WriteLine("Please Enter a positive integer.")
+            Console.ReadLine()
+
+        End If
+    End Sub
+End Class
---
+++ collatzconjecture2/collatzconjecture2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	float i = 0;
+
+	cout << "Enter a positive integer: ";
+	cin >> i;
+
+	if (i - (int)i == 0 && i > 0) {
+		int n;
+		n = i;
+
+		if (n > 0) {
+
+			while (n != 1) {
+
+				if (n % 2 == 0) {  // if the remainder of n/2 is 0,
+									  // that is, if n is even
+					n = n / 2;
+				}
+				else {
+					n = 3 * n + 1;
+				}
+
+				// print a space followed by the new number
+				cout << " " << n;
+			}
+			cout << endl;
+		}
+
+	}
+
+	else {
+		cout << "Please Enter a positive integer." << endl;
+	}
+
+	return 0;
+}
